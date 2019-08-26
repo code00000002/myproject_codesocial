fcb791cc07363c9cc90693d6532bedd2607b5e7b
code00000001 <1976331463@qq.com>
2019-08-26 12:47:59 +0800

    用户微服务-用户注册功能

---
+++ codesocial_user/src/main/java/com/codesocial/user/dao/AdminDao.java
@@ -0,0 +1,14 @@
+package com.codesocial.user.dao;
+
+import com.codesocial.user.pojo.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface AdminDao extends JpaRepository<Admin,String>,JpaSpecificationExecutor<Admin>{
+	
+}
---
+++ codesocial_user/src/main/java/com/codesocial/user/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.codesocial.user.dao;
+
+import com.codesocial.user.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface UserDao extends JpaRepository<User,String>,JpaSpecificationExecutor<User>{
+	
+}
